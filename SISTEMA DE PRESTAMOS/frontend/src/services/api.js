494de48c80c42d5d08bd9b6251134d279d2e005a
ArnoldSaya <asayar@ulasalle.edu.pe>
2026-07-10 00:04:16 -0500

    Corregir configuración de API

--- SISTEMA DE PRESTAMOS/frontend/src/services/api.js
+++ SISTEMA DE PRESTAMOS/frontend/src/services/api.js
@@ -50,4 +50,10 @@ api.interceptors.response.use(
   }
 )
 
+console.log("API URL:", import.meta.env.VITE_API_URL);
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || 'http://127.0.0.1:5000/api'
+});
+
 export default api
