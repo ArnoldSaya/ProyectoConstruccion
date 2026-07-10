12aba6649b55a470a06f62a5d93f359163c06da1
Arnold Daniel Saya Ramos <83090130+ArnoldSaya@users.noreply.github.com>
2026-07-10 00:31:42 -0500

    Delete SISTEMA DE PRESTAMOS directory

--- SISTEMA DE PRESTAMOS/backend/app/models/role.py
+++
@@ -1,7 +0,0 @@
-from . import db
-
-class Role(db.Model):
-    __tablename__ = 'roles'
-    
-    id = db.Column(db.Integer, primary_key=True)
-    role_name = db.Column(db.String(50), nullable=False, unique=True)
